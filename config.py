c82342192889b0046e6ec7181948cdceef03228a
Harshasbiabwinwi <harshvardhanprogamingi@gmail.com>
2025-12-19 09:23:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,16 +7,16 @@ elif os.path.exists("sample.env"):
     load_dotenv("sample.env")
 
 API_ID = os.getenv("26422668")
-API_HASH = os.getenv("13853df234b2fbe18d9027a5985cc69e")
+API_HASH = os.getenv("7033429094:AAGH6ybC-Sco8PS54qXJg-bQaDdy5oQk_EU")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 MONGO_DB_URI = os.getenv("mongodb+srv://villainravangaming:mikey_kun_781_@cluster0.fbgs1zz.mongodb.net/?retryWrites=true&w=majority")
 STATUS_MSG_ID = os.getenv("STATUS_MSG_ID")
 SCHEDULE_MSG_ID = os.getenv("SCHEDULE_MSG_ID")
-CHANNEL_TITLE = os.getenv("CHANNEL_TITLE", "AnimeDex")
+CHANNEL_TITLE = os.getenv("Anime dekho", "AnimeDex")
 INDEX_CHANNEL_USERNAME = os.getenv("https://t.me/Fx_Anime_Group")
 UPLOADS_CHANNEL_USERNAME = os.getenv("https://t.me/Fx_Anime_Group")
 TECHZ_API_KEY = os.getenv("TECHZ_API_KEY")
-COMMENTS_GROUP_LINK = os.getenv("COMMENTS_GROUP_LINK")
+COMMENTS_GROUP_LINK = os.getenv("https://t.me/Fx_Anime_Group")
 SLEEP_TIME = os.getenv("SLEEP_TIME", 60)
 
 for k, v in {
